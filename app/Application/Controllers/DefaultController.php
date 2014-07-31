8e2b20e4ad2396d1cb28227737d95f9f3535b5ac
kletellier <gregory.letellier@gmail.com>
2014-07-31 12:02:20 +0200

    add xls and pdf sample

--- app/Application/Controllers/DefaultController.php
+++ app/Application/Controllers/DefaultController.php
@@ -18,5 +18,35 @@ class DefaultController extends Controller
 	
 	}
         
+        public function xls()
+        {
+            // get Excel service from DI container
+            $objPHPExcel = $this->get('excel');            
+            $objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
+            $objPHPExcel->getProperties()->setLastModifiedBy("Maarten Balliauw");
+            $objPHPExcel->getProperties()->setTitle("Office 2003 XLS Test Document");
+            $objPHPExcel->getProperties()->setSubject("Office 2003 XLS Test Document");
+            $objPHPExcel->getProperties()->setDescription("Test document for Office 2003 XLS, generated using PHP classes.");
+
+            $objPHPExcel->setActiveSheetIndex(0);
+            $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello World');
+            $objPHPExcel->getActiveSheet()->setTitle('Simple');
+            
+            $contents = $objPHPExcel->GetBuffer();
+
+            $this->renderText($contents, "200", array('Content-Type' => 'application/vnd.ms-excel','Content-Disposition' => 'attachment; filename=excelfile.xls '));
+          
+        }
+        
+        public function pdf()
+        {
+            // get PDF service from DI container
+            $pdf = $this->get('pdf');
+            $pdf->AddPage();
+            $pdf->SetFont('Arial','B',16);
+            $pdf->Cell(40,10,'Hello World !');
+            $buffer = $pdf->GetBuffer();	
+            $this->renderText($buffer, "200", array('Content-Type' => 'application/pdf','Content-Disposition' => 'attachment; filename=pdffile.pdf '));
+        }
         
 }
