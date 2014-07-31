5d17c386e74e4fdca91e63ae32e70b6b7d425912
kletellier <gregory.letellier@gmail.com>
2014-07-31 11:59:29 +0200

    modify config.php path

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -3,6 +3,6 @@
 /**
  * Bootstrap application , load config file, start Eloquent ORM,  parse Request
  */
-require_once (ROOT . DS . 'config' . DS . 'config.php');
+require_once (ROOT . DS . 'library' . DS . 'config.php');
 require_once (ROOT . DS . 'library' . DS . 'database.php');
 require_once (ROOT . DS . 'library' . DS . 'shared.php');
