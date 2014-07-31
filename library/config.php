d9cf27cbb4b24611bed1b2bbb94f41ed4680a43b
kletellier <gregory.letellier@gmail.com>
2014-07-31 11:55:07 +0200

    Add xls and pdf sample, Add xls and pdf service in dependency container, Add configuration file in yml format

---
+++ library/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Define application constant
+ * DEVELOPMENT_ENVIRONMENT : boolean, to write error on output buffer or not
+ * BASE_PATH : website base url
+ * TWIG_CACHE : cache all twig templates for improving speed
+ * AUTORELOADCACHE :recreate twig cache for each request
+ */
+
+use Symfony\Component\Yaml\Parser;
+
+// loading configuration from config/config.yml
+$yaml = new Parser();
+$value = $yaml->parse(file_get_contents(CONFIGPATH));
+
+define('DEVELOPMENT_ENVIRONMENT',$value['debug']);
+define('BASE_PATH',$value['webpath']);
+define('TWIG_CACHE',$value['twig']['cache']);
+define('AUTORELOADCACHE',$value['twig']['alwaysreload']);
