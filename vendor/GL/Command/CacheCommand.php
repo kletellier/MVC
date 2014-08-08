d42df6a98e06725e0479b2e60142b318363eb7ba
unknown <gregory.letellier@gmail.com>
2014-08-08 16:38:14 +0200

    Add recursively delete all files before recreating folder

--- vendor/GL/Command/CacheCommand.php
+++ vendor/GL/Command/CacheCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Finder\Finder;
 
 class CacheCommand extends Command
 {
@@ -22,16 +23,22 @@ class CacheCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-	$fs = new Filesystem();
+        $fs = new Filesystem();
 
-		try {
-			$path = CACHEPATH . DS . "twig";
-			$fs->remove($path);			
-			$fs->mkdir($path);
-			$fs->chmod($path,0777,0000,true);
-		} catch (IOExceptionInterface $e) {
-			$output->writeln('Error : ' . $e->getMessage());
-		}		 
-		$output->writeln('finished');
+        try 
+        {
+                $path = CACHEPATH . DS . "twig";
+                $finder = new Finder();
+                $iterator = $finder->files()->name('*.php')->in($path);
+                $fs->remove($iterator);	
+                $fs->remove($path);		
+                $fs->mkdir($path);
+                $fs->chmod($path,0777,0000,true);
+        } 
+        catch (IOExceptionInterface $e) 
+        {
+                $output->writeln('Error : ' . $e->getMessage());
+        }		 
+        $output->writeln('finished');
     }
 }
