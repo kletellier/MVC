9af1256aef0cdf688295391fba332f41449f0358
kletellier <gregory.letellier@gmail.com>
2014-09-10 10:06:26 +0200

    Add security command to console

---
+++ vendor/GL/Command/SecurityCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace GL\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use GL\Core\SecurityService;
+
+class SecurityCommand extends Command
+{	 
+
+    protected function configure()
+    {
+        $this
+            ->setName('security:create')
+            ->setDescription('Create security users table and model') 
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {        
+        try 
+        {
+            $ss = new SecurityService(null,null);
+            $output->writeln('Create users table and model');
+            $ss->createTable();			 
+        } 
+        catch (\Exception $e) 
+        {
+                $output->writeln('Error : ' . $e->getMessage());
+        }		 
+        $output->writeln('finished');
+    }
+}
