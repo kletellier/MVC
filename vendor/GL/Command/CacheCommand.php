374a1a2c97edce6e0cbf9a23d0abffeac000f3f4
kletellier <gregory.letellier@gmail.com>
2014-09-24 10:10:18 +0200

    Add DI cached file clearing by cache:clear command

--- vendor/GL/Command/CacheCommand.php
+++ vendor/GL/Command/CacheCommand.php
@@ -17,17 +17,19 @@ class CacheCommand extends Command
     {
         $this
             ->setName('cache:clear')
-            ->setDescription('Clear all twig cache files') 
+            ->setDescription('Clear all application cache files') 
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Clear Twig cache files');
+        $output->writeln('Clear all application cache files');
         $fs = new Filesystem();
 
         try 
         {
+                $output->writeln("    Clear Twig Cache");
+
                 $path = CACHEPATH . DS . "twig";
                 $finder = new Finder();
                 $iterator = $finder->files()->name('*.php')->in($path);
@@ -35,6 +37,16 @@ class CacheCommand extends Command
                 $fs->remove($path);		
                 $fs->mkdir($path);
                 $fs->chmod($path,0777,0000,true);
+
+                $output->writeln("    Clear DI Cache");
+
+                $pathdi = CACHEPATH . DS . "DI" . DS . "Container";
+                $finderdi = new Finder();
+                $iteratordi = $finderdi->files()->name('*.php')->in($path);
+                $fs->remove($iteratordi); 
+                $fs->remove($pathdi);     
+                $fs->mkdir($pathdi);
+                $fs->chmod($pathdi,0777,0000,true);              
         } 
         catch (IOExceptionInterface $e) 
         {
