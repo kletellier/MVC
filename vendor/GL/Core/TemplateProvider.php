04ee45079a4fb2f472e16ed8bf8ddbf8969ab719
unknown <gregory.letellier@gmail.com>
2014-09-30 10:15:22 +0200

    Add template provider files

---
+++ vendor/GL/Core/TemplateProvider.php
@@ -0,0 +1,68 @@
+<?php 
+namespace GL\Core;
+
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
+ 
+class TemplateProvider  
+{   
+
+    protected $arr;
+    protected $instance;
+
+    public function __construct()
+    {        
+         
+        $this->arr = array();
+        $this->instance = null;
+        $this->loadFile();
+    }     
+
+    public function loadFile()
+    {
+        $ret = false;
+        try 
+        {
+            $path = TEMPLATECONF; 
+            $fs = new Filesystem();
+            $exist = $fs->exists(array($path));
+
+            if($exist)
+            {
+                // add shared TwigHelper
+                $yaml = new Parser();
+                $value = $yaml->parse(file_get_contents(TEMPLATECONF)); 
+                foreach($value as $name => $th)
+                {
+                    $this->arr['name'] = $th['class'];                    
+                }
+            }           
+        } 
+        catch (IOException $e) {
+            $ret = false;
+        }
+        catch (Exception $e) {
+            $ret = false;
+        }   
+        return $ret;
+    }
+
+    public function getTemplateService()
+    {
+        $ret = null;
+        try {
+            if(isset($this->arr[TEMPLATE_ENGINE]))
+            {
+                $class =  $this->arr[TEMPLATE_ENGINE];
+                $ret = new $class;
+            }
+        } catch (Exception $e) {
+            
+        }
+        return $ret;
+    }
+
+     
+    
+}
