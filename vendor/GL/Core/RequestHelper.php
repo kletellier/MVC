e0a152137df03fd8af43f063e0912fca3b6da84e
kletellier <gregory.letellier@gmail.com>
2014-12-10 14:55:10 +0100

    Add MobileDectect component

--- vendor/GL/Core/RequestHelper.php
+++ vendor/GL/Core/RequestHelper.php
@@ -27,15 +27,8 @@ class RequestHelper
      */
     function isMobile()
     {               
-        $useragent = $this->_request->headers->get('User-Agent');
-        //var_dump($useragent);
-        $regex_match="/(nokia|iphone|android|motorola|^mot\-|softbank|foma|docomo|kddi|up\.browser|up\.link|";
-        $regex_match.="htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|";
-        $regex_match.="blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam\-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|";
-        $regex_match.="symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte\-|longcos|pantech|gionee|^sie\-|portalmmm|";
-        $regex_match.="jig\s browser|hiptop|^ucweb|^benq|haier|^lct|opera\s*mobi|opera\*mini|320x320|240x320|176x220";
-        $regex_match.=")/i";
-        return  preg_match($regex_match, strtolower($useragent));
+        $md = new \Detection\MobileDetect($this->_request->headers->all(),$this->_request->headers->get('User-Agent'));
+        return $md->isMobile();
     }
     
     /**
