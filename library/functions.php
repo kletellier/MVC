d768136bd4d499c667f1480dc3be4795ff71192d
kletellier <gregory.letellier@gmail.com>
2015-01-29 14:56:47 +0100

    Add sdump function

--- library/functions.php
+++ library/functions.php
@@ -1,8 +1,11 @@
 <?php
- 
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+
 if ( ! function_exists('ddump'))
 {
-	 /**
+	  /**
 	 * Dump the passed variables and end the script.
 	 *
 	 * @param  mixed
@@ -13,3 +16,19 @@ if ( ! function_exists('ddump'))
 		array_map(function($x) { var_dump($x); }, func_get_args()); die;	 
 	}
 }
+
+if ( ! function_exists('sdump'))
+{
+	  /**
+	 * Dump the passed variables and end the script.
+	 *
+	 * @param  mixed
+	 * @return void
+	 */
+	function sdump()
+	{	 
+		array_map(function($x) { dump($x); }, func_get_args()); die;	 
+	}
+}
+
+
