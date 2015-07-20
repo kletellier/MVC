81e70b63793316b82f32612b0b4e199a154f8ed8
kletellier <gregory.letellier@gmail.com>
2015-07-20 12:09:45 +0200

    Replace renderText by renderDownload function

--- app/Application/Controllers/DefaultController.php
+++ app/Application/Controllers/DefaultController.php
@@ -5,47 +5,44 @@ use GL\Core\Controller\Controller as Controller;
 
 class DefaultController extends Controller
 {
-	public function hello($name)
-	{                              
-		$text = "Hello " .$name . " !";
-		return $this->render('index.html.twig',array('text'=>$text));	
-	}
-	
-	public function testdb()
-	{
-		$test = \Application\Models\Test::all();
-		return $this->renderJSON($test);
-	
-	}
-        
-        public function xls()
-        {
-            // get Excel service from DI container
-            $objPHPExcel = $this->get('excel');            
-            $objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
-            $objPHPExcel->getProperties()->setLastModifiedBy("Maarten Balliauw");
-            $objPHPExcel->getProperties()->setTitle("Office 2003 XLS Test Document");
-            $objPHPExcel->getProperties()->setSubject("Office 2003 XLS Test Document");
-            $objPHPExcel->getProperties()->setDescription("Test document for Office 2003 XLS, generated using PHP classes.");
+    public function hello($name)
+    {                              
+        $text = "Hello " .$name . " !";
+        return $this->render('index.html.twig',array('text'=>$text));   
+    }
+    
+    public function testdb()
+    {
+        $test = \Application\Models\Test::all();
+        return $this->renderJSON($test);       
+    }
+    
+    public function xls()
+    {
+        // get Excel service from DI container
+        $objPHPExcel = $this->get('excel');            
+        $objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
+        $objPHPExcel->getProperties()->setLastModifiedBy("Maarten Balliauw");
+        $objPHPExcel->getProperties()->setTitle("Office 2003 XLS Test Document");
+        $objPHPExcel->getProperties()->setSubject("Office 2003 XLS Test Document");
+        $objPHPExcel->getProperties()->setDescription("Test document for Office 2003 XLS, generated using PHP classes.");
 
-            $objPHPExcel->setActiveSheetIndex(0);
-            $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello World');
-            $objPHPExcel->getActiveSheet()->setTitle('Simple');
-            
-            $contents = $objPHPExcel->GetBuffer();
-
-            return $this->renderText($contents, "200", array('Content-Type' => 'application/vnd.ms-excel','Content-Disposition' => 'attachment; filename=excelfile.xls '));
-          
-        }
+        $objPHPExcel->setActiveSheetIndex(0);
+        $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello World');
+        $objPHPExcel->getActiveSheet()->setTitle('Simple');
         
-        public function pdf()
-        {
-            // get PDF service from DI container
-            $pdf = $this->get('pdf');
-            $pdf->AddPage();            
-            $pdf->Cell(40,10,'Hello World !');
-            $buffer = $pdf->GetOuput();	
-            return $this->renderText($buffer, "200", array('Content-Type' => 'application/pdf','Content-Disposition' => 'attachment; filename=pdffile.pdf '));
-        }
-        
+        $contents = $objPHPExcel->GetBuffer();
+        return $this->renderDownload($contents,"file.xls");
+    }
+    
+    public function pdf()
+    {
+        // get PDF service from DI container
+        $pdf = $this->get('pdf');
+        $pdf->AddPage();            
+        $pdf->Cell(40,10,'Hello World !');
+        $buffer = $pdf->GetOuput(); 
+        return $this->renderDownload($buffer,"file.pdf");
+    }
+    
 }
