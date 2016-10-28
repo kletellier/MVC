93418be523856f20b64b0853f782fb1f80bbb948
kletellier <gregory.letellier@gmail.com>
2016-10-28 11:22:28 +0200

    refactor error response method

--- vendor/GL/Core/Controller/ControllerResolver.php
+++ vendor/GL/Core/Controller/ControllerResolver.php
@@ -16,13 +16,13 @@ use Assert\AssertionFailedException;
  */
 class ControllerResolver
 {
-    protected $_controller;
-    protected $_action;  
-    protected $_args;
-    protected $_errors;
-    protected $_container;
-    protected $_errorcontroller;
-    
+  protected $_controller;
+  protected $_action;  
+  protected $_args;
+  protected $_errors;
+  protected $_container;
+  protected $_errorcontroller;
+
     /**
      * Constructor
      * 
@@ -31,187 +31,143 @@ class ControllerResolver
      * @param Array $args Arguments extracted from route
      * 
      */
-  function __construct($controller, $action,$args,$container=null) 
-        {
-            $this->_controller = ucfirst($controller);
-            $this->_action = $action;
-            $this->_args = $args;
-            $this->_errors = array();             
-            $this->_container = $container;            
-            $this->_errorcontroller = $this->getErrorController();
-      }     
+    function __construct($controller, $action,$args,$container=null) 
+    {
+      $this->_controller = ucfirst($controller);
+      $this->_action = $action;
+      $this->_args = $args;
+      $this->_errors = array();             
+      $this->_container = $container;            
+      $this->_errorcontroller = $this->getErrorController();
+    }     
 
-      private function getErrorController()
-      {
-          $error = \Parameters::get('error');
-          $ctl = (isset($error["controller"])===true) ? $error["controller"] : $this->getControllerName("error"); 
-          return $ctl;
-      } 
+    private function getErrorController()
+    {
+      $error = \Parameters::get('error');
+      $ctl = (isset($error["controller"])===true) ? $error["controller"] : $this->getControllerName("error"); 
+      return $ctl;
+    } 
 
-      public function setContainer($container)
-      {
-        $this->_container = $container;
+    public function setContainer($container)
+    {
+      $this->_container = $container;
+    }
+
+    public function addDebug($str)
+    {
+      if(DEVELOPMENT_ENVIRONMENT){
+        $this->_container->get('debug')["messages"]->addMessage($str);
       }
 
-        public function addDebug($str)
-        {
-            if(DEVELOPMENT_ENVIRONMENT){
-                $this->_container->get('debug')["messages"]->addMessage($str);
-            }
-           
-        }  
+    }  
 
-        /**
-         * 
-         * Return Error 405 Response
-         *         
-         */
-        private function get405Response()
-        {
-            ob_clean();
-            $this->_controller = "error";
-            $this->_action = "error405";
-            $this->_args = array();             
-            return $this->execute();           
-        }
-               
-         /**
-         * 
-         * Return Error 404 Response
-         *         
-         */
-        private function get404Response()
-        {
-            ob_clean();
-            $this->_controller = "error";
-            $this->_action = "error404";
-            $this->_args = array();             
-            return $this->execute();           
-        }
-        
-        /**
-         * Return Errror 430 response
-         */
-        private function get403Response()
-        {
-            ob_clean();
-            $this->_controller = "error";
-            $this->_action = "error403";
-            $this->_args = array();             
-            return $this->execute(); 
-        }
-        
-          /**
-         * Return Errror 401 response
-         */
-        private function get401Response()
-        {
-            ob_clean();
-            $this->_controller = "error";
-            $this->_action = "error401";
-            $this->_args = array();             
-            return $this->execute(); 
-        }
-        
+    private function getErrorResponse($code)
+    {
+      ob_clean();
+      $this->_controller = "error";
+      $this->_action = "error$code";
+      $this->_args = array();             
+      return $this->execute();    
+    }
 
-        /**
-         * Translate error message
-         * @param string $code message code to translate
-         * @param string $default default value to display
-         * @return string string translated in locale
-         */
-        function trans($code,$default)
+    /**
+     * Translate error message
+     * @param string $code message code to translate
+     * @param string $default default value to display
+     * @return string string translated in locale
+     */
+    function trans($code,$default)
+    {
+      $ret = $default;
+      if($this->_container!=null)
+      {
+        $ts = $this->_container->get('translator');
+        $tmp = $ts->translate($code);
+        if($tmp!="")
         {
-          $ret = $default;
-          if($this->_container!=null)
-          {
-            $ts = $this->_container->get('translator');
-            $tmp = $ts->translate($code);
-            if($tmp!="")
-            {
-              $ret=$tmp;
-            }
-          }
-          return $ret;
-        }  
-        
-      /**
-       * 
-       * Function return Controller namespace
-       * 
-       * @return String Controller namespace
-       */
-        private function getControllerName()
-        {
-            switch (strtolower($this->_controller)) 
-            {
-              case 'error':
-                return $this->_errorcontroller;
-                break;
-              
-              default:
-                return 'Application\\Controllers\\'.ucfirst($this->_controller).'Controller';
-                break;
-            }
-            
+          $ret=$tmp;
         }
+      }
+      return $ret;
+    }  
         
+    /**
+     * 
+     * Function return Controller namespace
+     * 
+     * @return String Controller namespace
+     */
+    private function getControllerName()
+    {
+      switch (strtolower($this->_controller)) 
+      {
+        case 'error':
+        return $this->_errorcontroller;
+        break;
+
+        default:
+        return 'Application\\Controllers\\'.ucfirst($this->_controller).'Controller';
+        break;
+      }
+
+    }
+
+   /**
+    * 
+    * Return a controller instance
+    * 
+    * @param String $controllername Controller namespace to instanciate
+    * @return \GL\Core\Controller\Controller Herited instance of controller
+    */
+   private function getInstance($controllername)
+   {           
+    try 
+    {
+      Assertion::ClassExists($controllername);
+    } 
+    catch (AssertionFailedException $e) {
+      echo "Error class $controllername does not exists";
+      die();
+    }
+    $instance =  new $controllername($this->_controller,$this->_action);
+          // add dependency container in the controller instance
+    $instance->setContainer($this->_container);
+    return $instance;
+  }
+
      /**
+      * Function to extract required parameters for action with route paramaters extracted
       * 
-      * Return a controller instance
-      * 
-      * @param String $controllername Controller namespace to instanciate
-      * @return \GL\Core\Controller\Controller Herited instance of controller
+      * @param \GL\Core\Controller\Controller $instance Controller instance
+      * @return Array Arguments array for execute action function
       */
-        private function getInstance($controllername)
-        {           
-            try 
-            {
-              Assertion::ClassExists($controllername);
-            } 
-            catch (AssertionFailedException $e) {
-              echo "Error class $controllername does not exists";
-              die();
-            }
-            $instance =  new $controllername($this->_controller,$this->_action);
-            // add dependency container in the controller instance
-            $instance->setContainer($this->_container);
-            return $instance;
-        }
-        
-       /**
-        * Function to extract required parameters for action with route paramaters extracted
-        * 
-        * @param \GL\Core\Controller\Controller $instance Controller instance
-        * @return Array Arguments array for execute action function
-        */
-        private function getArguments($instance)
+     private function getArguments($instance)
+     {
+      $r = new \ReflectionMethod($instance, $this->_action);                        
+      $arguments = $this->RetrieveArguments($this->_args,$r->getParameters());
+      return $arguments;
+    }
+
+     /**
+      * Extract arguments from route parsing parameters
+      * 
+      * @param array $attributes required parameters for action function
+      * @param array $parameters extracted from route parsing
+      * @return array
+      */
+     private function RetrieveArguments(array $attributes, array $parameters)
+     { 
+      $arguments = array();
+      foreach ($parameters as $param) 
+      {
+        if (array_key_exists($param->name, $attributes))
         {
-            $r = new \ReflectionMethod($instance, $this->_action);                        
-            $arguments = $this->RetrieveArguments($this->_args,$r->getParameters());
-            return $arguments;
-        }
-               
-       /**
-        * Extract arguments from route parsing parameters
-        * 
-        * @param array $attributes required parameters for action function
-        * @param array $parameters extracted from route parsing
-        * @return array
-        */
-        private function RetrieveArguments(array $attributes, array $parameters)
-        { 
-            $arguments = array();
-            foreach ($parameters as $param) 
-            {
-                if (array_key_exists($param->name, $attributes))
-                {
-                    $arguments[] = $attributes[$param->name];
-                }  
-            }
-            return $arguments;
-        }                   
-        
+          $arguments[] = $attributes[$param->name];
+        }  
+      }
+      return $arguments;
+    }                   
+
       /**
        * Execute action in controller and render only text (no response)
        * 
@@ -219,74 +175,74 @@ class ControllerResolver
        * 
        * @return string
        */
-        function render()
-        {
+      function render()
+      {
+        $ret = "";
+        try 
+        {       
+          $controllerName = $this->getControllerName();
+          $dispatch = $this->getInstance($controllerName);
+          if ((int)method_exists($controllerName, $this->_action)) 
+          {               
+            $ret = call_user_func_array(array($dispatch,$this->_action),$this->getArguments($dispatch));        
+          } else 
+          {
             $ret = "";
-            try 
-            {       
-                $controllerName = $this->getControllerName();
-                $dispatch = $this->getInstance($controllerName);
-                if ((int)method_exists($controllerName, $this->_action)) 
-                {               
-                    $ret = call_user_func_array(array($dispatch,$this->_action),$this->getArguments($dispatch));        
-                } else 
-                {
-                    $ret = "";
-                } 
-            }
-            catch(Exception $ex)
-            {
-                    $ret = "";
-            }
-            return $ret;
-        }              
-  
+          } 
+        }
+        catch(Exception $ex)
+        {
+          $ret = "";
+        }
+        return $ret;
+      }              
+
       /**
        * Execute action in selected controller (normally return a Response object)
        */
       function execute()
       {        
-            $response = null;
-            try 
-            {       
-                $controllerName = $this->getControllerName();
-                $dispatch = $this->getInstance($controllerName);  
-            
-                $this->addDebug("Controller : " . $controllerName);
-                $this->addDebug("Action : " . $this->_action);                   
+        $response = null;
+        try 
+        {       
+          $controllerName = $this->getControllerName();
+          $dispatch = $this->getInstance($controllerName);  
 
-                if ((int)method_exists($controllerName, $this->_action)) 
-                { 
-                    $params = $this->getArguments($dispatch); 
-                    $response = call_user_func_array(array($dispatch,$this->_action),$params);                   
-                }
-                else 
-                {
-                    $response = $this->get404Response();                   
-                } 
+          $this->addDebug("Controller : " . $controllerName);
+          $this->addDebug("Action : " . $this->_action);                   
 
-            }
-            catch(\GL\Core\Exception\AccessDeniedHttpException $ad)
-            {
-                $response = $this->get401Response();
-            }
-            catch(\GL\Core\Exception\AccessForbiddenHttpException $ad)
-            {
-                $response = $this->get403Response();
-            }
-            catch(\GL\Core\Exception\NotFoundHttpException $nf)
-            {
-                $response = $this->get404Response();
-            }  
-             catch(\GL\Core\Exception\MethodNotAllowedException $mna)
-            {
-                $response = $this->get405Response();
-            }          
-            return $response;
-     }        
-  
-     function __destruct() 
+          if ((int)method_exists($controllerName, $this->_action)) 
+          { 
+            $params = $this->getArguments($dispatch); 
+            $response = call_user_func_array(array($dispatch,$this->_action),$params);                   
+          }
+          else 
+          {
+            $response = $this->getErrorResponse(404);                   
+          } 
+
+        }
+        catch(\GL\Core\Exception\AccessDeniedHttpException $ad)
         {
-    
-       }
+          $response = $this->getErrorResponse(401);
+        }
+        catch(\GL\Core\Exception\AccessForbiddenHttpException $ad)
+        {
+          $response = $this->getErrorResponse(403);
+        }
+        catch(\GL\Core\Exception\NotFoundHttpException $nf)
+        {
+          $response = $this->getErrorResponse(404);
+        }  
+        catch(\GL\Core\Exception\MethodNotAllowedException $mna)
+        {
+          $response = $this->getErrorResponse(405);
+        }          
+        return $response;
+      }        
+
+    function __destruct() 
+    {
+
+    }
 }
