71ad76534e932eeda7ae7af434f7894a037b7255
kletellier <gregory.letellier@gmail.com>
2017-07-31 14:27:40 +0200

    missing error controller interface

--- vendor/GL/Core/Controller/ControllerErrorInterface.php
+++ vendor/GL/Core/Controller/ControllerErrorInterface.php
@@ -4,14 +4,6 @@ namespace GL\Core\Controller;
 
 interface ControllerErrorInterface 
 { 	
-    public function error500($message,$file,$line,$errors);
-
-    public function error405();
-
-    public function error404();
-
-    public function error401();
-
-    public function error403();
+    public function error($code);
     
 }
